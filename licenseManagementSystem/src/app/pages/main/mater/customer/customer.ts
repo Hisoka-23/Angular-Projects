4da625f63159881897b3e7a1324b6e49f176acee
prakash <prakash.chaurasiya214522@gmail.com>
2025-09-03 10:12:38 +0530

    commit

--- licenseManagementSystem/src/app/pages/main/mater/customer/customer.ts
+++ licenseManagementSystem/src/app/pages/main/mater/customer/customer.ts
@@ -1,13 +1,59 @@
-import { Component } from '@angular/core';
+import { CustomerList } from './../../../../interface/customer-List';
+import { Component, inject, signal, TemplateRef } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { NgxDatatableModule, ColumnMode } from '@swimlane/ngx-datatable';
+import { MatIconModule } from '@angular/material/icon';
+import { BsModalRef, BsModalService, ModalModule } from 'ngx-bootstrap/modal';
+import { ContentHeader } from "../../../../widgets/content-header/content-header";
+import { CUSTOMER } from '../../../../mock-data/customer.list.mock';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-customer',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [
+    NgxDatatableModule,
+    MatButtonModule,
+    MatIconModule,
+    ModalModule,
+    ContentHeader
+  ],
+  providers:[BsModalService],
   templateUrl: './customer.html',
   styleUrl: './customer.css'
 })
-export class Customer {
+export class Customer implements OnInit {
+
+  title = 'CUSTOMER';
+
+  customers = signal<CustomerList[]>([]);
+
+  modalRef = signal<BsModalRef | null>(null);
+
+  private modalService = inject(BsModalService);
+
+  ColumnMode = ColumnMode;
+
+  ngOnInit() {
+    console.log('ngoninit customer');
+    this.customers.set(CUSTOMER);
+  }
+
+  onPageChange(event: any) {
+    console.log(event);
+  }
+
+  onSortChange(event: any) {
+    console.log(event);
+  }
+
+    openUserFormModal(template: TemplateRef<void>, User?: CustomerList){
+  
+      this.modalRef.set(this.modalService.show(template, { class: 'modal-lg' }));
+    }
+
+      closeUserModal(){
+    this.modalRef()?.hide();
+  }
 
 }
--- licenseManagementSystem/src/app/pages/main/view/customer-list/customer-list.ts
+++ licenseManagementSystem/src/app/pages/main/view/customer-list/customer-list.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-customer-list',
+  standalone: true,
   imports: [],
   templateUrl: './customer-list.html',
   styleUrl: './customer-list.css'
 })
 export class CustomerList {
+  load(arg0: string) {
+    throw new Error('Method not implemented.');
+  }
 
 }
