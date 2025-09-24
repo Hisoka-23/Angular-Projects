b1c86d79d836e9462601c0fd91dfc0296f034829
prakash <prakash.chaurasiya214522@gmail.com>
2025-09-24 18:10:22 +0530

    login and sidebar add

--- licenseManagementSystem/src/app/interface/menu-item.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface MenuItem{
-    label: string;
-    icon?: string;
-    route?: string;
-    submenu?: MenuItem[];
-    isHeader?: boolean;
-}
--- licenseManagementSystem/src/app/interface/party-List.ts
+++
@@ -1,9 +0,0 @@
-export interface PartyList{
-    Customer: string;
-    ContactPerson: string;
-    Email: string;
-    Mobile: number;
-    Country: string;
-    State: string;
-    City: string;
-}
